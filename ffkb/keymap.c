3f6c50851c4714c8cdc6dfa30d3fc19790f900a6
Dean Verleger <deanverleger@gmail.com>
2022-04-19 17:19:55 -0700

    add encoders. fix gui thumbs

--- ffkb/keymap.c
+++ ffkb/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______ , GUI__L2           ,           GUI__R2 , _______ ,
    _______ , GUI__L3           ,           GUI__R3 , _______ ,
 //---------+---------                     ---------+---------//
-   GUIx_LQ , _______ , GUIx__L , GUIx__R , _______ , GUIx_RQ ,
+   GUIx_LQ , GUIx__L , GUIx_LT , GUIx_RT , GUIx__R , GUIx_RQ ,
    _______
 //---------+---------//
 ),
@@ -156,5 +156,41 @@ layer_state_t layer_state_set_keymap(layer_state_t state) {
       break;
   }
   return state;
+}
+#endif
+
+#ifdef ENCODER_ENABLE
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    // default behavior if undefined
+    if (index == 0) {
+        // Conditional to reverse the direction of encoder
+        #ifdef ENCODERS_A_REVERSE
+        if (!clockwise) {
+        #else
+        if (clockwise) {
+        #endif
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    else if (index == 1) {
+      // Conditional to reverse the direction of encoder
+      #ifdef ENCODERS_B_REVERSE
+      if (!clockwise) {
+      #else
+      if (clockwise) {
+      #endif
+        tap_code16(_BRUP__);
+      }
+      else{
+        tap_code16(_BRDWN_);
+      }
+    } else if (index == 3) {
+      // center feature
+    }
+
+    return true;
 }
 #endif
