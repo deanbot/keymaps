eccc55fdfffe4b38e5a52e312a8d7fac59112e1b
Dean Verleger <deanverleger@gmail.com>
2022-08-22 08:51:58 -0700

    add tri layer state

--- oxymoron/keymap.c
+++ oxymoron/keymap.c
@@ -82,3 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _NAV, _SYM, _NUM);
+}
